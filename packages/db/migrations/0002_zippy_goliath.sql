d0527044ff811d07542d8720a879bf26d31fe832
VTECH <vtech@VTECHdeMac-mini.local>
2026-06-12 22:33:12 +0900

    chore(rust): P4-T1 删除 TS 版(apps/packages/旧部署物/旧 CI)与工程文件清理

--- packages/db/migrations/0002_zippy_goliath.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "usage_records" DROP CONSTRAINT "usage_records_key_id_api_keys_id_fk";
--- packages/shared/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./api-key.js";
-export * from "./password.js";
-export * from "./envelope.js";
-export * from "./cost.js";
-export * from "./events.js";
-export * from "./redis-keys.js";
